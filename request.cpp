7df63d032f34d4efe9c557a7427d76ffb8961496
hsaktiwy <hsaktiwy@gmail.com>
2024-01-14 02:04:46 +0100

    uri parsing and path existence nearly done

--- request.cpp
+++ request.cpp
@@ -5,6 +5,28 @@ request::request()
 
 }
 
+static bool CharacterUri(char c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == ':'
+		|| (c == '0' && c == '9') ||   c == '/' || c == ';' || c == '?'
+		|| c == '#' || c == '$' || c == '-' || c == '_' || c == '.' 
+		|| c == '+' || c == '!' || c == '*' || c == '\'' || c == '('
+		|| c == ')' || c == ',' || c == '~' || c == '@' || c == '&'
+		|| c == '=' || c == '%' ||  c == '[' || c == ']' )
+		return (true);
+	return (false);
+}
+
+static bool CheckUriFormat(std::string &uri)
+{
+	for(int i = 0; uri[i]; i++)
+	{
+		if (!CharacterUri(uri[i]))
+			return (false);
+	}
+	return (true);
+}
+
 // using this fucntion we can avoid adding the SP and HT plus LWS to our request string and replace them all bye a space
 static void replaceConsecutiveSpaces(std::string &HTTPrequest, std::string &req)
 {
@@ -81,7 +103,6 @@ static bool CheckForBody(std::string &request, size_t index)
 
 static void	ParseHeaders(std::vector<HTTPHeader> &headers, std::string &request, std::string &body, size_t &index, bool &error, int &status)
 {
-	std::string KnownHeaders[] = {"Host", "Accept", "Accept-Language", "Accept-Encoding", "Connection", "Referer"};
 	std::string tmp;
 	std::string delimiter = "\r\n";
 	bool stop = false;
@@ -179,6 +200,8 @@ static void	MethodParsing(bool &error, int &status, std::string &HTTPrequest, st
 	// check the validity of the uri
 	//  			?
 	// check the http protocol
+	if (!CheckUriFormat(uri))
+		error = true, status = 400;
 	if (tmp[i] == ' ')
 		i++;
 	while (tmp[i] && tmp[i] != ' ')
@@ -213,12 +236,11 @@ void	request::ParseRequest(char *r)
 	std::cout << "Before Request form :\n" << req <<std::endl;
 	replaceConsecutiveSpaces(HTTPrequest, req);
 	// Define the method  ?
-	MethodParsing(error, status, HTTPrequest, method, uri, http, index);
+	MethodParsing(error, status, HTTPrequest, method, method_uri, http, index);
 	// parsthe headers
 	ParseHeaders(headers, req, body, index, error, status);
 	GetRequestHost(headers, host);
 	// files, plus check the syntaxe
-	RequestDisplay();
 }
 
 static bool absoluteURI(std::string &uri)
@@ -232,58 +254,154 @@ static bool absoluteURI(std::string &uri)
 	return (false);
 }
 
-static bool CharacterUri(char c)
+static void	FillUriStructor(t_uri& uri, std::string &full_uri, bool authority)// authority boolean will confirme if there is a authority in the uri or note
 {
-	if ((c == 'a' && c == 'z') || (c == 'A' && c == 'Z') || c == ':'
-		|| (c == '0' && c == '9') ||   c == '/' || c == ';' || c == '?'
-		|| c == '#' || c == '$' || c == '-' || c == '_' || c == '.'
-		|| c == '+' || c == '!' || c == '*' || c == '\'' || c == '('
-		|| c == ')' || c == ',' || c == '~' || c == '@' || c == '&'
-		|| c == '=' || c == '%')
-		return (true);
-	return (false);
+	size_t size = 0;
+	size += uri.scheme.size(); 
+	// extract the authority
+	if (authority)
+	{
+		for (size_t i = size; full_uri[i] && full_uri[i] != '/' && full_uri[i] != '?' && full_uri[i] != '#'; i++)
+			uri.authority += full_uri[i];
+		size += uri.authority.size();
+	}
+	if (full_uri[size] && full_uri[size] == '/')
+		size++;
+	// extract the path
+	for (size_t i = size; full_uri[i] && full_uri[i] != '?' && full_uri[i] != '#'; i++)
+		uri.path += full_uri[i];
+	size += uri.path.size();
+	if (full_uri[size] && full_uri[size] == '?')
+		size++;
+	// extract the query
+	for(size_t i = size; full_uri[i] && full_uri[i] != '#'; i++)
+		uri.query += full_uri[i];
+	size += uri.query.size();
+	if (full_uri[size] && full_uri[size] == '#')
+		size++;
+	// extract the fragment  i think we will remove this line and it logic in the future
+	for (size_t i = size; full_uri[i]; i++)
+		uri.fragment += full_uri[i];
 }
 
-static bool CheckUriFormat(std::string &uri)
+static void UriFormat(t_uri &uri, std::string &full_uri, std::string &root, std::string &host)// 1 for absolute 2 for relative 3 for autority 
 {
-	for(int i = 0; uri[i]; i++)
+	// check it the uri start with http or https
+	//[scheme]://[authority]/[path]?[query]#[fragment]
+	bool	absoluteUri = absoluteURI(full_uri);
+	if (absoluteUri == true)
 	{
-		if (!CharacterUri(uri[i]))
+		// in case if the uri is absolute
+		if (full_uri.find("http:://") == 0)
+		{
+			// extract the shceme
+			uri.scheme = "http:://";
+			FillUriStructor(uri, full_uri, true);
+		}
+		else
+		{
+			// extract the shceme
+			uri.scheme = "https:://";
+			FillUriStructor(uri, full_uri, true);
+		}
+		uri.type = ABSOLUTE;
+	}
+	else if (full_uri.find(host) == 0)
+		FillUriStructor(uri, full_uri, true), uri.type = AUTHORITY;
+	else
+		FillUriStructor(uri, full_uri, false), uri.type = RELATIVE;
+}
+
+bool	ft_strcmp(const char *s1, const char *s2)
+{
+	for(int i = 0; s1[i] || s2[i]; i++)
+	{
+		if (s1[i] != s2[i])
 			return (false);
 	}
 	return (true);
 }
 
-static URI UriFormat(std::string &uri, std::string &root)// 1 for absolute 2 for relative 3 for autority 
+static bool	CheckPathExistance(t_uri &uri, std::string root)
 {
-	std::string path;
-	// check it the uri start with http or https
-	//[scheme]://[authority]/[path]?[query]#[fragment]
-	bool	absoluteUri = absoluteURI(uri);
-	if (absoluteUri == true)
+	DIR *dir;
+	struct dirent *dirent;
+	std::string src,path;
+	std::string file;
+	src = "./" + root + "/" + uri.path;
+	path = src;
+	size_t s = path.rfind("/");
+	if (s != std::string::npos)
+		path = path.substr(0, s);
+	std::cout << " new Path  ? : " << path << std::endl;
+	dir = opendir(path.c_str());
+	if (dir)
 	{
-		if (uri.find("http:://") == 0)
-			path = uri.substr(8);
+		if (s != std::string::npos)
+		{
+			std::string file = src.substr(s + 1);
+			std::cout << "File named " << file << std::endl;
+			bool stop = false;
+			while (!stop)
+			{
+				dirent = readdir(dir);
+				if (dirent)
+				{
+					std::cout << "in" << std::endl;
+					if (dirent->d_type == DT_REG)
+					{
+						std::cout << dirent->d_name << std::endl;
+						if (ft_strcmp(dirent->d_name, file.c_str()))
+						{
+							if (closedir(dir) == -1)
+								std::cerr << "Error \n	closedir Failed" << std::endl;
+							return (true);
+						}
+					}
+				}
+				else
+					stop = true;
+			}
+		}
 		else
-			path = uri.substr(9);
+		{
+			if (closedir(dir) == -1)
+				std::cerr << "Error \n	closedir Failed" << std::endl;
+			return (true);
+		}
 	}
+	return (false);
 }
 
 void	request::CheckRequest(std::vector<ServerBlocks> &serverBlocks)
 {
+	std::string KnownHeaders[] = {"Host", "Accept", "Accept-Language", "Accept-Encoding", "Connection", "Referer"};
 	ServerBlocks block = get_server_block(host, serverBlocks);
 	std::string root =  get_root(block.getDirectives());
 	std::string mimeType[] = {"image/avif", "image/avif", "image/jpeg", "image/gif", "image/png", "text/csv",  "text/html",   "text/javascript", "text/plain", "text/xml", "text/plain", "audio/mpeg", "video/mp4", "video/mpeg", "application/xml"};
 
-	// check if the uri is available
-	bool uri_parse = CheckUriFormat(uri);
-	
+	if (error == false)
+	{
+		std::cout << "parse URI" << std::endl;
+		// splite uri to scheme, authority, path, query
+		UriFormat(uri, method_uri, root, host);
+		if (uri.path.size() == 0)
+			
+		if (!CheckPathExistance(uri, root))
+		{
+			error = true, status = 404;
+			// return ;
+		}
+		// Check for known headers
+
+	}
+	RequestDisplay();
 }
 
 void	request::RequestDisplay( void )
 {
 	std::cout << "Request :\n" << req <<std::endl;
-	std::cout << "methode : " << method << ", uri " << uri << ", http protocol : " << http << " hostname : " << host << " error " << error << " status " << status <<std::endl;
+	std::cout << "methode : " << method << ", uri " << method_uri << ", http protocol : " << http << " hostname : " << host << " error " << error << " status " << status <<std::endl;
 	for (std::vector<HTTPHeader>::iterator iter = headers.begin();  iter != headers.end(); iter++)
 	{
 		std::cout <<"--> "<< iter->name << " : ";
@@ -295,6 +413,8 @@ void	request::RequestDisplay( void )
 	}
 	std::cout << "Body : \n";
 	std::cout << body;
+	std::cout << "Parsed URI : \n";
+	std::cout << "Uri scheme : " << uri.scheme  << ",URI authority " << uri.authority  << ",URI path " << uri.path << ",URI query " << uri.query << ",URI fragment " << uri.fragment << std::endl;
 }
 
 request::~request()
--- request.hpp
+++ request.hpp
@@ -6,6 +6,8 @@
 #include <iostream>
 #include <list>
 #include <math.h>
+#include <sys/types.h>
+#include <dirent.h>
 #include "http.server.hpp"
 
 // special character
@@ -19,45 +21,57 @@
 // httpheader structor that willl hold the entty-header (name and values)
 typedef struct t_HTTPHeader
 {
-    std::string             name;
-    std::list<std::string>  values;
-    bool                    error;
-    int                     status;
+	std::string				name;
+	std::list<std::string>	values;
+	bool					error;
+	int						status;
 } HTTPHeader;
 
 // enum that will have the type of method used
 typedef  enum{
-    GET,
-    POST,
-    DELETE
-}   Method;
+	GET,
+	POST,
+	DELETE
+}	Method;
 
 typedef  enum{
-    ABS,
-    REL,
-    AUTO
-}  URI;
+	ABSOLUTE,
+	RELATIVE,
+	AUTHORITY
+}	URI_Type;
+
+// hier-part [ "?" query ] [ "#" fragment ]
+typedef struct Uri
+{
+	URI_Type	type;
+	std::string	scheme;
+	std::string	authority;// this is the host
+	std::string	path; // our path
+	std::string	query; // query
+	std::string	fragment;
+} t_uri;
 
 class request {
     private:
-        std::string method;
-        std::string uri;
-        std::string http;
-        std::string host;
-        std::vector<HTTPHeader> headers;
-        std::string body;
-        std::string req;
-        bool    error;
-        int     status;
+		std::string				method;
+		std::string				method_uri;
+		t_uri					uri;
+		std::string				http;
+		std::string				host;
+		std::vector<HTTPHeader>	headers;
+		std::string				body;
+		std::string				req;
+		bool					error;
+		int						status;
     public:
-        request();
-        ~request();
-        request(const request& copy);
-        void ParseRequest(char *request);
-        void CheckRequest(std::vector<ServerBlocks> &serverBlocks);// THIS WILL CHECK THE REQUEST VALIDITY
-        request& operator=(const request& obj);
-        Method getMethod( void ) const; // to get the method when we need it
-        const std::vector<std::string>& getHeaders( void ) const;// get the headers after being prased
-        void    RequestDisplay( void );
+		request();
+		~request();
+		request(const request& copy);
+		void							ParseRequest(char *request);
+		void							CheckRequest(std::vector<ServerBlocks> &serverBlocks);// THIS WILL CHECK THE REQUEST VALIDITY
+		request&						operator=(const request& obj);
+		Method							getMethod( void ) const; // to get the method when we need it
+		const std::vector<std::string>&	getHeaders( void ) const;// get the headers after being prased
+		void							RequestDisplay( void );
 };
 #endif
